da6684145b9eee353752f094d6c02e52a5275e32
AlexeySmirnov93 <voinvetra92@mail.ru>
2022-10-18 20:36:48 +0300

    добавили программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа

---
+++ Console_App5/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+Console.WriteLine("Введите чило: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int min = 99;
+int max = 1000;
+if (n > min && n < max)
+{
+    int result = n % 10;
+    Console.WriteLine( $"Последняя цифра трехзначного числа: {result}");
+}
+else
+{
+    Console.WriteLine($"Данное число не является трехзначным: {n}");
+}
